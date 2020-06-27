d63637b40615803a560e5cda75cb4355884cedb8
Shinjini Ghosh <shinghos@mit.edu>
2020-06-27 02:01:34 -0400

    added tokenization

--- imdb_sentiment_analysis.py
+++ imdb_sentiment_analysis.py
@@ -7,6 +7,9 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=FutureWarning)
     import tensorflow as tf
     import tensorflow_datasets as tfds
+    from tensorflow import keras
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 # print(tf.__version__)
@@ -31,3 +34,23 @@ print("Generating testing data")
 for sentence, label in tqdm(testing_data):
     testing_sentences.append(str(sentence.numpy()))
     testing_labels.append(str(label.numpy()))
+
+training_labels = np.array(training_labels)
+testing_labels = np.array(testing_labels)
+
+
+# tokenizing
+vocab_size = 10000
+embedding_dim = 16
+max_length = 150
+trucation_type = 'post'
+oov_token = "<OOV>"
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded = pad_sequences(sequences, maxlen=max_length, truncating=trucation_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sequences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length)
